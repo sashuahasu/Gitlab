27710de2ef60d49d3c26bdda47660ce16102431c
root <root@ubuntu-git.asia-south1-c.c.ferrous-terrain-249115.internal>
2019-08-21 11:52:52 +0000

    commit everything Updated shell bash for second time

--- shell.bash
+++ shell.bash
@@ -8,6 +8,7 @@ echo edited by indrapal
 =======
 echo edited by Mr tiwari
 date
+<<<<<<< HEAD
 >>>>>>> 4ffd428... Update shell.bash
 
 =======
@@ -16,3 +17,7 @@ date
 echo edited second time
 date
 >>>>>>> f085bfad32f861ab8cc43dd7a5e2948b8b3912ef
+=======
+echo edited second time
+date
+>>>>>>> 3e87b9b... Updated shell bash for second time
